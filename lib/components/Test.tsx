9ad1777d574db209496e0474bd1bf34f5a04c8eb
Pawel <pawel.kuznik@gmail.com>
2024-07-31 11:12:52 +0200

    Initial steps. Nearly works.

---
+++ lib/components/Test.tsx
@@ -0,0 +1,10 @@
+
+export interface TestProps {
+    name: string;
+};
+
+export function Test({ name }: TestProps) {
+    return (
+        <span>Test</span>
+    );
+};
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      include: ["lib"],
+      copyDtsFiles: true,
+      outDir: "dist"
+    }),
+  ],
+  build: {
+    copyPublicDir: false,
+    rollupOptions:{
+      external: ['react', 'react/jsx-runtime']
+    },
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: [ "es" ],
+      fileName: "main"
+    }
+  }
+})
