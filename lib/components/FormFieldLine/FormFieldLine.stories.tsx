d7cdb1041891a05f384f6b8b1a092076625c2e5e
Pawel <pawel.kuznik@gmail.com>
2025-06-22 10:23:44 +0200

    Adding widths to FormFieldLine

--- lib/components/FormFieldLine/FormFieldLine.stories.tsx
+++ lib/components/FormFieldLine/FormFieldLine.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { FormFieldLine } from "../FormFieldLine";
+import { FormFieldLine } from "./FormFieldLine";
 import { Input } from "../Input";
 
 export default {
@@ -11,6 +11,20 @@ type Story = StoryObj<typeof FormFieldLine>;
 
 export const DefaultNormal: Story = {
     args: {
+        children: (
+            <>
+                <Input/>
+                <Input/>
+                <span style={{ width: "1ch"}}>x</span>
+                <Input/>
+            </>
+        )
+    }
+};
+
+export const WithWidths: Story = {
+    args: {
+        widths: [ '100px', '100px', 'auto' ],
         children: (
             <>
                 <Input/>
--- lib/components/FormFieldLine/FormFieldLine.tsx
+++ lib/components/FormFieldLine/FormFieldLine.tsx
@@ -1,12 +1,28 @@
-import { ReactNode } from "react";
+import { PropsWithChildren } from "react";
 
-export interface FormFieldLineProps {
-    children: ReactNode;
+export interface FormFieldLineProps extends PropsWithChildren {
+    /**
+     *  An array CSS values as `px` or `auto` units.
+     */
+    widths?: string[];
 };
 
-export function FormFieldLine({ children }: FormFieldLineProps) {
+/**
+ *  This is a component that can place input in one line. The widths of
+ *  the fields can be controlled with the widths property.
+ */
+export function FormFieldLine({ children, widths }: FormFieldLineProps) {
+
+    const styles : { gridTemplateColumns?: string } = { }
+
+    if (widths) {
+        styles.gridTemplateColumns = widths.join(" ")
+    }
+
+    console.log('styles', widths, styles);
+
     return (
-        <div className="faceplate-formfieldline">
+        <div className="faceplate-formfieldline" style={styles}>
             {children}
         </div>
     );
